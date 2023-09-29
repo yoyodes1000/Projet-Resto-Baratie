b090562722b3671944bd0de030124de40fae4c87
Vicky KOCKMANN <kockmann.victoria@gmail.com>
2023-09-29 15:54:55 +0200

    test

---
+++ _contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets/css/contact.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+
+    <?php
+include "_navbar.php";
+    ?>
+
+
+<body>
+Le Baratie<br> 
+    Nous trouver 
+    <h1>Nous contacter</h1>
+    <p>Téléphone: <br>
+    05.19.00.00.00 <br> <br>
+    Email: <br>
+    lebaratie@eastblue.com</p>
+
+    <img src="mapcontact.png"
+    alt="map au tresor">
+
+    <h2>Reservation</h2>
+    <h3>Heures d'ouverture</h3>
+    <p>Tous les jours de la semaine <br>
+    à toute heure de la journée</p>
+
+    
+
+</body>
+</html>
