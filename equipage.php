f9a6e3c29dc0a07ed08a711f2fffcf4051aea6e5
Didixe <Adeline.gilet@gmail.com>
2023-10-02 08:50:51 +0200

    modified equipage.php

--- equipage.php
+++ equipage.php
@@ -7,15 +7,15 @@
     <link rel="stylesheet" href="assets/css/equipages.css">
     <title>Equipages</title>
 </head>
+<body>
 <?php
     include "_navbar.php";
 ?>
-<body>
     <section class="teamPresentation" >
         <h1>L'Equipages</h1>
             <div class="containerImageLeft" >
                 <div class="imageLeft">
-                    <img class="pictureVictoria" src="assets/Images/VictoriaSmall.jpg" alt="Picture of Victoria">
+                    <img class="picture" src="assets/Images/VictoriaSmall.jpg" alt="Picture of Victoria">
                 </div>
                 <div class="presentationRight">
                     <p class="presentationVictoria" >Victoria la tatouée.<br> Femme indépendante, navigatrice du Baratie, elle a déjà parcouru les mers d'East Blue ! Sur son dos, vous trouverez la carte du monde. Mais attention elle ne se laisse pas marcher sur les pieds, ne la cherchez pas ou vous allez la trouver ! </p>
@@ -23,7 +23,7 @@
             </div>
             <div class="containerImageRight">
                     <div class="imageRight">
-                        <img class="pictureJohann" src="assets/Images/JohannSmall.jpg" alt="Picture of Johann">
+                        <img class="picture" src="assets/Images/JohannSmall.jpg" alt="Picture of Johann">
                     </div>
                     <div class="presentationLeft">
                         <p class="presentationJohann">Johann le décontracté. <br> Ancien croupier, il a déjà eu affaire au pirate Capone Bege et s'en est sorti avec seulement quelques égratignures. Très bon cuisinier Il s'est fait remarquer par Zeff grâce à ses délicieuses râmen.</p>
@@ -31,7 +31,7 @@
             </div>
             <div class="containerImageLeft">
                 <div class="imageLeft">
-                    <img class="pictureMelissa" src="assets/Images/MelissaSmall.jpg" alt="Picture of Mélissa">
+                    <img class="picture" src="assets/Images/MelissaSmall.jpg" alt="Picture of Mélissa">
                 </div>
                 <div class="presentationRight">
                     <p class="presentationMelissa">Mélissa la Dompteuse <br>. Aucun animal ne lui résiste! Toujours accompagnée de ses fidèles molosses Kaïdo et Rainbow, elle ne se bat presque jamais, et quand c'est nécessaire, elle utilise son fouet !</p>
@@ -39,7 +39,7 @@
             </div>
             <div class="containerImageRight">
                 <div class="imageRight">
-                    <img class="pictureEtienne" src="assets/Images/EtienneSmall.jpg" alt="Picture of Etienne">
+                    <img class="picture" src="assets/Images/EtienneSmall.jpg" alt="Picture of Etienne">
                 </div>
                 <div class="presentationLeft">
                     <p class="presentationEtienne">Etienne le matelot. <br> Ancien lieutenant de la Marine, Etienne s'est fait renvoyer après avoir dérobé et mangé le Bara Bara no Mi, le fruit du démon de la fragmentation, qui permet à son utilisateur d'être immunisé contre les attaques coupantes et d'être capable de séparer les différentes parties de son corps et de les faire léviter.</p>
@@ -47,7 +47,7 @@
             </div>
             <div class="containerImageLeft">
                 <div class="imageLeft">
-                    <img class="pictureAdeline" src="assets/Images/AdelineSmall.jpg" alt="Picture of Adeline">
+                    <img class="picture" src="assets/Images/AdelineSmall.jpg" alt="Picture of Adeline">
                 </div>
                 <div class="presentationRight">
                     <p class="presentationAdeline">Adeline la frileuse .<br> Vous la trouverez toujours avec un plaid; son principal ennemi : l'hiver. C'est un comble quand on sait qu'elle a grandi sur l'île de Drum , l'île hivernale de Grand Line qui a vu naître le célèbre Tony Tony Chooper.  </p>
