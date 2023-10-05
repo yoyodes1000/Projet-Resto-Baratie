c8810dae52c9b1b61f120558dfcf21b82e17dfb0
yoyodes1000 <yoyodes1000@gmail.com>
2023-10-05 16:44:31 +0200

    pimpoye

--- index.php
+++ index.php
@@ -51,21 +51,16 @@
                 Sanji est un cuistot dévoué qui croit que la cuisine doit être préparée avec passion et dévouement. Il refuse de gaspiller de la nourriture et s'efforce toujours de fournir des repas de qualité, même dans les situations les plus difficiles. Sa philosophie culinaire s'inspire de son rêve de trouver le "All Blue", une légendaire mer où tous les ingrédients de toutes les mers se rejoignent pour créer la cuisine ultime.
                 </p>
             </div>
-        
 
         <main>
             
         
         </main>
-        <footer>
+
         <?php
 
            include ("footer.php");
 
         ?>
-
-
-        </footer>
-        
-        
+    
     </html>
