ddf8adc4f6c0977781d30ee75d3a91be97d8a23b
unknown <yoyodes1000@gmail.com>
2023-10-05 11:40:05 +0200

    quand je suis content je commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/footer.css">
+</head>
+<body>
+    <footer>
+            <div class="left">&#xa9;WCS-2023</div>
+            <div class="right">
+                <a href="#"><img src="assets/Images/logo-facebook.png" alt="logo facebook" class="facebook"></a>
+                <a href="#"><img src="assets/Images/logo-instagram.png" alt="logo instagram" class="instagram"></a>
+            </div>
+    </footer>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <footer class="map">
+            <div class="left">&#xa9;WCS-2023</div>
+            <div class="right">
+                <a href="#"><img src="assets/Images/logo-facebook.png" alt="logo facebook" class="facebook"></a>
+                <a href="#"><img src="assets/Images/logo-instagram.png" alt="logo instagram" class="instagram"></a>
+            </div>
+    </footer>
+</body>
+</html>
