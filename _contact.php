2e82aecd9bda854588542c0e24505f95d3e44bd8
Vicky KOCKMANN <kockmann.victoria@gmail.com>
2023-10-05 12:22:01 +0200

    modif

--- _contact.php
+++ _contact.php
@@ -1,35 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="assets/css/contact.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact</title>
+  <link rel="stylesheet" href="assets/css/contact.css">
 </head>
 
-    <?php
-include "_navbar.php";
-    ?>
+<?php
+    include "_navbar.php";
+?>
 
+<br> 
+<br>
+<br> 
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br>
+<br>
+<br>
 
 <body>
-Le Baratie<br> 
-    Nous trouver 
-    <h1>Nous contacter</h1>
-    <p>Téléphone: <br>
-    05.19.00.00.00 <br> <br>
-    Email: <br>
-    lebaratie@eastblue.com</p>
+  <div class="container">
+    <h1>Le Baratie</h1>
+    <h2>Téléphone : 05.19.00.00.00</h2>
+    <h2>Email : lebaratie@eastblue.com</h2>
 
-    <img src="mapcontact.png"
-    alt="map au tresor">
 
-    <h2>Reservation</h2>
-    <h3>Heures d'ouverture</h3>
-    <p>Tous les jours de la semaine <br>
-    à toute heure de la journée</p>
+<br> 
+<br>
+<br> 
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br>
+<br>
+<br>
 
-    
+    <button class="Réservation">Réservation</button>
+
+    <h4>Heures d'Ouverture :</h4>
+    <p>Tous les jours de la semaine à toute heure de la journée</p>
+
+<br> 
+<br>
+<br> 
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br>
+<br>
+<br>    
+
+    <img src="assets/Images/map_contact.jpg"
+      alt="map au tresor">
+
+  </div>
+
+<br> 
+<br>
+<br> 
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br> 
+<br>
+<br>
+<br>
 
 </body>
 </html>
