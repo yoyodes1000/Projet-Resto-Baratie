1c05b00b8e84bf8f2e8e678b08e69c896ecdcdbf
Didixe <Adeline.gilet@gmail.com>
2023-10-05 12:11:28 +0200

    changement style text one piece sur le h1 style.css - et des class sur les img du footer

--- footer.php
+++ footer.php
@@ -7,19 +7,19 @@
     <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
-    <footer>
+    <!-- <footer>
             <div class="left">&#xa9;WCS-2023</div>
             <div class="right">
-                <a href="#"><img src="assets/Images/logo-facebook.png" alt="logo facebook" class="facebook"></a>
-                <a href="#"><img src="assets/Images/logo-instagram.png" alt="logo instagram" class="instagram"></a>
+                <a href="#"><img class="imgFooter" src="assets/Images/logo-facebook.png" alt="logo facebook" class="facebook"></a>
+                <a href="#"><img class="imgFooter" src="assets/Images/logo-instagram.png" alt="logo instagram" class="instagram"></a>
             </div>
     </footer>
-            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
     <footer class="map">
             <div class="left">&#xa9;WCS-2023</div>
             <div class="right">
-                <a href="#"><img src="assets/Images/logo-facebook.png" alt="logo facebook" class="facebook"></a>
-                <a href="#"><img src="assets/Images/logo-instagram.png" alt="logo instagram" class="instagram"></a>
+                <a href="#"><img class="imgFooter" src="assets/Images/logo-facebook.png" alt="logo facebook" class="facebook"></a>
+                <a href="#"><img class="imgFooter" src="assets/Images/logo-instagram.png" alt="logo instagram" class="instagram"></a>
             </div>
     </footer>
 </body>
