5b6fb814b2dbaf6be4009f202ca086d4bfaf759e
ETIENNE BUCHHOLD <etienne.buchhold@hotmail.com>
2023-10-05 14:24:18 +0200

    voila

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'accueil</title>
     <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap" rel="stylesheet">
     
 
 </head>
+
+<?php
+
+include ("_navbar.php");
+
+?>
 
     </header>   
  <div class="title">
@@ -35,6 +42,7 @@ Le Baratie vous accueille dans une atmosphère chaleureuse et conviviale. Notre 
 <div class=floating>
     <img  class = sanji src="assets/images/imagesSANJI.jpeg" alt="Photo de Sanji">
 </div>
+    
     <div class=sanji-presentation>
         <p>
         Sanji est un cuistot dévoué qui croit que la cuisine doit être préparée avec passion et dévouement. Il refuse de gaspiller de la nourriture et s'efforce toujours de fournir des repas de qualité, même dans les situations les plus difficiles. Sa philosophie culinaire s'inspire de son rêve de trouver le "All Blue", une légendaire mer où tous les ingrédients de toutes les mers se rejoignent pour créer la cuisine ultime.
@@ -48,6 +56,11 @@ Le Baratie vous accueille dans une atmosphère chaleureuse et conviviale. Notre 
     </main>
 
     <footer>
+    <?php
+
+include ("footer.php");
+
+?>
+</footer>
     
-    </footer>
     
